7ee8cc2e6f4b0134e58da79153a7d8764aaea907
Surendar PD <80924716+surendar-pd@users.noreply.github.com>
2023-10-19 19:08:02 +0530

    added: humans

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/home/Hero";
@@ -23,28 +22,4 @@ export default function Home() {
             </main>
         </div>
     );
-=======
-import Head from 'next/head';
-import Header from '@/components/Header';
-import Hero from '@/components/home/Hero';
-import Features from '@/components/home/Features';
-import Category from '@/components/home/Category';
-import Faqs from '@/components/home/Faqs';
-
-export default function Home() {
-	return (
-		<div className='flex min-h-screen w-full bg-[#161616] text-stone-200'>
-			<Head>
-				<title>DeWorks</title>
-			</Head>
-			<main className='w-full font-work'>
-			  <Header />
-				<Hero />
-				<Features />
-				<Category />
-				<Faqs />
-			</main>
-		</div>
-	);
->>>>>>> a64553fc450a2e7934e2fb3d2e91e93e3a86a32f
 }
