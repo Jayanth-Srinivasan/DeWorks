9b1848f1c6e3a489e21985bda7fb005acc9fb03d
Kunal Keshan <kunalkeshan12@gmail.com>
2023-10-20 14:12:19 +0530

    chore: updated legal links in footer

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -70,25 +70,17 @@ function Footer() {
 						<div className='flex flex-col items-center justify-start md:flex-row md:justify-between'>
 							<div className='mb-4 flex flex-col items-stretch justify-center text-center md:flex-row'>
 								<Link
-									href='/terms-and-conditions'
+									href='/terms'
 									className='py-2 text-[14px] text-slate-300 md:mr-[24px]'
 								>
 									Terms and Conditions
 								</Link>
 								<Link
-									href='/privacy-policy'
+									href='/privacy'
 									className='py-2 text-[14px] text-slate-300 md:mr-[24px]'
 								>
 									Privacy Policy
 								</Link>
-								<a
-									target='_blank'
-									rel='noopener noreferrer'
-									href='https://blog.codelancedevs.com'
-									className='py-2 text-[14px] text-slate-300 md:mr-[24px]'
-								>
-									Blog
-								</a>
 							</div>
 							<p className='mb-4 text-[12px] text-slate-300'>
 								Copyright &copy; 2023 DeWorks
