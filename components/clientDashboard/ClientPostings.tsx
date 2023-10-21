77fa9e238c2e1bc05727cfee81410282c501293c
Kunal Keshan <kunalkeshan12@gmail.com>
2023-10-21 11:39:26 +0530

    chore: using ethers utils

--- components/clientDashboard/ClientPostings.tsx
+++ components/clientDashboard/ClientPostings.tsx
@@ -1,50 +1,56 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "../ui/button";
-import Image from "next/image";
-import { CalendarRange } from "lucide-react";
-import { useRouter } from "next/router";
-import { getAllPostData } from "@/blockchain/constants/utils";
-import { formatEther } from "ethers";
+import React, { useEffect, useState } from 'react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import { CalendarRange } from 'lucide-react';
+import { useRouter } from 'next/router';
+import { getAllPostData } from '@/blockchain/constants/utils';
+import { utils } from 'ethers';
 
 function ClientPostings() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const [postings, setPostings] = useState<Posting[]>([]);
+	const [postings, setPostings] = useState<Posting[]>([]);
 
-  useEffect(() => {
-    getAllPostData().then((data) => setPostings(data as Posting[]));
-  }, []);
+	useEffect(() => {
+		getAllPostData().then((data) => setPostings(data as Posting[]));
+	}, []);
 
-  console.log(postings);
+	console.log(postings);
 
-  return (
-    <section className="mt-4 font-outfit">
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {postings.map((post, idx) => (
-          <div
-            className="w-full flex flex-col lg:flex-ro md:justify-between md:items-center gap-8 hover:-translate-y-1 transition-all duration-300 bg-app-grey-light p-4 md:p-8 rounded border border-white/10"
-            key={idx}
-          >
-            <div className="flex flex-col gap-4">
-              <h2 className="bg-app-slate-blue rounded font-medium px-2 py-1 w-fit">
-                {post.category}
-              </h2>
-              <h1 className="font-semibold text-2xl">{post.title}</h1>
-              <div className="text-base flex items-center gap-4">
-                <p>
-                  <span className="font-medium">
-                    {/* @ts-ignore */}
-                    {formatEther(post.payInMATIC)}
-                  </span>{" "}
-                  MATIC
-                </p>
-                <p>•</p>
-                <p className="flex items-center gap-2">
-                  <CalendarRange strokeWidth={1.5} size={18} /> 2 days ago
-                </p>
-              </div>
-            </div>
-            {/* <div className="flex gap-4 items-center">
+	return (
+		<section className='mt-4 font-outfit'>
+			<div className='mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+				{postings.map((post, idx) => (
+					<div
+						className='w-full flex flex-col lg:flex-ro md:justify-between md:items-center gap-8 hover:-translate-y-1 transition-all duration-300 bg-app-grey-light p-4 md:p-8 rounded border border-white/10'
+						key={idx}
+					>
+						<div className='flex flex-col gap-4'>
+							<h2 className='bg-app-slate-blue rounded font-medium px-2 py-1 w-fit'>
+								{post.category}
+							</h2>
+							<h1 className='font-semibold text-2xl'>
+								{post.title}
+							</h1>
+							<div className='text-base flex items-center gap-4'>
+								<p>
+									<span className='font-medium'>
+										{/* @ts-ignore */}
+										{utils.formatEther(post.payInMATIC)}
+									</span>{' '}
+									MATIC
+								</p>
+								<p>•</p>
+								<p className='flex items-center gap-2'>
+									<CalendarRange
+										strokeWidth={1.5}
+										size={18}
+									/>{' '}
+									2 days ago
+								</p>
+							</div>
+						</div>
+						{/* <div className="flex gap-4 items-center">
               <div>
                 <Image
                   unoptimized
@@ -64,18 +70,20 @@ function ClientPostings() {
                 </div>
               </div>
             </div> */}
-            <Button
-              onClick={() => router.push(`/dashboard/client/posting/${idx}`)}
-              variant={"secondary"}
-              className="h-12 w-full text-base"
-            >
-              View Posting
-            </Button>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+						<Button
+							onClick={() =>
+								router.push(`/dashboard/client/posting/${idx}`)
+							}
+							variant={'secondary'}
+							className='h-12 w-full text-base'
+						>
+							View Posting
+						</Button>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 }
 
 export default ClientPostings;
