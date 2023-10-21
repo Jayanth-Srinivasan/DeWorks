1d66f2077684cf51693cf7a3cb56d1338577f078
Kunal Keshan <kunalkeshan12@gmail.com>
2023-10-21 07:15:56 +0530

    Feat/users fetch (#19)
    * added new contract and abi
    
    * Merge conflicts fixed
    
    * Added the helper function and changed the abi, contract address
    
    ---------
    
    Co-authored-by: chanakyha <vetrichanakyha2003@gmail.com>

--- blockchain/constants/utils.ts
+++ blockchain/constants/utils.ts
@@ -3,15 +3,18 @@ import ABI, { contractAddress } from "./usersABI";
 
 const rpcUrl =
   "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78";
-const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
-const wallet = new ethers.Wallet(process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY!, provider);
+const provider = new ethers.JsonRpcProvider(rpcUrl);
+const wallet = new ethers.Wallet(
+  process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY!,
+  provider
+);
+const contract = new ethers.Contract(contractAddress, ABI, wallet);
 export default provider;
 
 async function checkUserExists(address: string) {
   try {
-    const contract = new ethers.Contract(contractAddress, ABI, provider);
     const userExists = await contract.userExists(address);
-    console.log(userExists)
+    console.log(userExists);
 
     if (userExists[0]) {
       console.log(
@@ -34,7 +37,7 @@ async function addClientData(
   companyName: string,
   description: string,
   email: string,
-  walletAddress: string,
+  walletAddress: string
 ) {
   try {
     const contract = new ethers.Contract(contractAddress, ABI, wallet);
@@ -59,7 +62,7 @@ async function addFreelancerData(
   category: string,
   skills: string,
   experience: string,
-  walletAddress: string,
+  walletAddress: string
 ) {
   try {
     const contract = new ethers.Contract(contractAddress, ABI, wallet);
@@ -69,7 +72,7 @@ async function addFreelancerData(
       walletAddress,
       category,
       experience,
-      skills,
+      skills
     );
     await transaction.wait();
     console.log("Client data added successfully.");
@@ -80,4 +83,72 @@ async function addFreelancerData(
   }
 }
 
-export { checkUserExists, addClientData, addFreelancerData };
+const postJob = async (
+  title: string,
+  location: string,
+  category: string,
+  payInMATIC: number,
+  experience: string,
+  description: string
+) => {
+  try {
+    const valueInWei = ethers.parseEther(payInMATIC.toString());
+
+    const tx = await contract.postJob(
+      title,
+      location,
+      category,
+      valueInWei,
+      experience,
+      description,
+      {
+        value: valueInWei,
+      }
+    );
+
+    await tx.wait();
+    console.log("Job posted successfully!");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+async function requestJob(jobId: number) {
+  try {
+    const tx = await contract.requestJob(jobId);
+    await tx.wait();
+    console.log("Job requested successfully!");
+  } catch (error) {
+    console.error("Error requesting job: " + error);
+  }
+}
+
+async function acceptJobRequest(jobId: string) {
+  try {
+    const tx = await contract.acceptJobRequest(jobId);
+    await tx.wait();
+    console.log("Job request accepted successfully!");
+  } catch (error) {
+    console.error("Error accepting job request: " + error);
+  }
+}
+
+async function completeJob(jobId: number) {
+  try {
+    const tx = await contract.completeJob(jobId);
+    await tx.wait();
+    console.log("Job completed successfully!");
+  } catch (error) {
+    console.error("Error completing job: " + error);
+  }
+}
+
+export {
+  checkUserExists,
+  addClientData,
+  addFreelancerData,
+  postJob,
+  requestJob,
+  acceptJobRequest,
+  completeJob,
+};
